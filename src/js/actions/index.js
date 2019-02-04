2cacdac2500913892ff0760858432fd9c8bf1ed2
Pedro Hernandez <pherned@gmail.com>
2019-02-04 10:53:07 -0500

    added CREATE_FIB_ARRAY action and action type constant

---
+++ src/js/actions/index.js
@@ -0,0 +1,7 @@
+// src/js/actions/index.js
+
+import { CREATE_FIB_ARRAY } from '../constants/action-types';
+
+export function createFibArray(payload) {
+    return { type: CREATE_FIB_ARRAY, payload}
+};
---
+++ src/js/constants/action-types.js
@@ -0,0 +1,3 @@
+// src/js/constants/action-types.js
+
+export const CREATE_FIB_ARRAY = "CREATE_FIB_ARRAY";
